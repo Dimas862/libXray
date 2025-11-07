e5527cf21e8c11d78b0f9b149ffc79b776641850
zdm <zakharov@barl.ru>
2025-11-07 15:29:56 +0300

    disable memory limit (test)

--- memory/memory_ios.go
+++ memory/memory_ios.go
@@ -2,30 +2,4 @@
 
 package memory
 
-import (
-	"runtime/debug"
-
-	"time"
-)
-
-const (
-	interval = 1
-	// 30M
-	maxMemory = 25 * 1024 * 1024
-)
-
-func forceFree(interval time.Duration) {
-	go func() {
-		for {
-			time.Sleep(interval)
-			debug.FreeOSMemory()
-		}
-	}()
-}
-
-func InitForceFree() {
-	debug.SetGCPercent(10)
-	debug.SetMemoryLimit(maxMemory)
-	//duration := time.Duration(interval) * time.Second
-	//forceFree(duration)
-}
+func InitForceFree() {}
