b45ecd1056a504e0ac6e69bc07a6b42dbddd5ddd
zdm <zakharov@barl.ru>
2026-03-31 18:24:55 +0300

    Revert "disable memory limit (test)"
    This reverts commit e5527cf21e8c11d78b0f9b149ffc79b776641850.
    
    Revert "comment duration"
    
    This reverts commit e5f634b8f95501f08b3eb61e0ff50c55b3cbb261.
    
    Revert "set memory limit to 25Mb and disable forceFree"
    
    This reverts commit 410a137ca2ab1946e4feaccde93b75b8d21b27e7.
    
    Revert "change memory to 5 Mb (test)"
    
    This reverts commit c3f7eff5418188f4aec4e5825778618640a468af.
    
    Revert "increase limit memory to 50 Mb (test)"
    
    This reverts commit 5b758ddba8936840dc00635a00952ec337b6e5d8.

--- memory/memory_ios.go
+++ memory/memory_ios.go
@@ -2,4 +2,30 @@
 
 package memory
 
-func InitForceFree() {}
+import (
+	"runtime/debug"
+
+	"time"
+)
+
+const (
+	interval = 1
+	// 30M
+	maxMemory = 15 * 1024 * 1024
+)
+
+func forceFree(interval time.Duration) {
+	go func() {
+		for {
+			time.Sleep(interval)
+			debug.FreeOSMemory()
+		}
+	}()
+}
+
+func InitForceFree() {
+	debug.SetGCPercent(10)
+	debug.SetMemoryLimit(maxMemory)
+	duration := time.Duration(interval) * time.Second
+	forceFree(duration)
+}
